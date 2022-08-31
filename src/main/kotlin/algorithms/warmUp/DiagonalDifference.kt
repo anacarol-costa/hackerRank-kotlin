9d5913b1d6ee479d2cc2d2345db05fc6d2a49c23
anacarol.costa <carol.costa2502@gmail.com>
2022-08-30 22:41:12 -0300

    solucionar diagonal difference

---
+++ src/main/kotlin/algorithms/warmUp/DiagonalDifference.kt
@@ -0,0 +1,63 @@
+package algorithms.warmUp
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    var n = arr.size
+    var diagonalEsquerda = 0
+    var diagonalDireita = 0
+
+    for (i in arr.indices ) {
+
+        for (j in arr.indices) {
+
+            if (i == j) {
+                diagonalEsquerda += arr[i][j]
+            }
+
+            if (i + j == n -1) {
+                diagonalDireita += arr[i][j]
+            }
+        }
+
+    }
+    return Math.abs(diagonalEsquerda - diagonalDireita)
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
